b06a2955bb2769244655eb83419432f889d188a8
Hilmi K <90126618+raiho80@users.noreply.github.com>
2023-01-14 18:23:14 +0800

    Update reg_streamlit.py

--- reg_streamlit.py
+++ reg_streamlit.py
@@ -42,6 +42,9 @@ colnames = boruta_X
 # split by 80-20, random_state=10
 X_train, X_test, Y_train, Y_test = train_test_split(boruta_X, Y, test_size = 0.2, random_state = 10)
 
+
+## Decision Tree
+
 #creating decision tree regressor model
 regressorDT = DecisionTreeRegressor(splitter='best', max_depth = 3)
 regressorDT.fit(X_train, Y_train)
@@ -61,6 +64,30 @@ tree.plot_tree(regressorDT,
                feature_names = fn,  
                filled = True);
 
+
+## Linear Regression
+
+#creating linear regression model
+regressorLR = LinearRegression()
+regressorLR.fit(X_train, Y_train)
+
+Y_pred_lr = np.round(regressorLR.predict(X_test),4)
+
+df_lr = pd.DataFrame({'Actual value' : Y_test, 'Predicted value' : Y_pred_lr, 'Difference' : Y_test - Y_pred_lr})  
+df_lr.sort_index()
+
+d = "Mean squared error : ", round(sm.mean_squared_error(Y_test, Y_pred_lr), 4)
+e = "Mean absolute error : ", round(sm.mean_absolute_error(Y_test, Y_pred_lr), 4)
+f = "r2 score : ", r2_score(Y_test, Y_pred_lr)
+
+#Visualizing actual value and predicted value of model
+plt.figure(figsize = (10, 12))
+figLR = plt.scatter(Y_test, Y_pred_lr)
+plt.xlabel('Actual')
+plt.ylabel('Predicted')
+plt.title('Actual Money Spent vs Predicted Money Spent (in RM) ')
+
+
 with output:
     st.header("Decision Tree")
     st.text("Q1")
@@ -71,7 +98,7 @@ with output:
 
     st.header("Linear Regression")
     st.text("Q2")
-    st.write(a)
-    st.write(b)
-    st.write(a)
-    st.write(b)
+    st.write(d)
+    st.write(e)
+    st.write(f)
+    st.pyplot(fig=figLR.figure, clear_figure=None)
